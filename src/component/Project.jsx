d388f6d35e4bbc2b137ed3fd8bc0bff347b44201
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-03-28 14:57:56 -0700

    update

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -8,11 +8,12 @@ const Project = () => {
     <section id="project" className="bg-gray-800 py-20">
       <div className="container mx-auto px-4">
         <p className="text-gray-400 text-center">Portfolio</p>
-        <h2 className="text-3xl font-bold text-center text-white mt-2"><span className="text-orange-500">Latest</span> Project</h2>
+        <h2 className="text-3xl font-bold text-center text-white mt-2"><span className="
+        text-yellow-500">Latest</span> Project</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
           <div className="bg-gray-900 p-4 rounded-lg">
             <img src={img1} alt="project" className="rounded-lg" />
-            <p className='text-orange-500 mt-4  text-3xl'>Rental House  </p>
+            <p className='text-yellow-500 mt-4  text-3xl'>Rental House  </p>
             <h1 className=' text-white mt-2 text-xl'>The project is built with React js ,Tailwindcss ,Node 
               js and MongoDB</h1>   
           </div>
