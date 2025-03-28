e7bda89e4e1b863506969a60f301df480da8d08c
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-03-28 15:06:00 -0700

    update

--- src/component/Service.jsx
+++ src/component/Service.jsx
@@ -6,7 +6,7 @@ const Service = () => {
       <div className="container mx-auto px-4 ">
         <p className="text-gray-400 text-center">What I Offer!</p>
         <h2 className="text-3xl font-bold text-center text-white mt-2">
-          <span className="text-yellow-500">My</span> Services</h2>
+          <span className=" text-yellow-500">My</span> Services</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8     animate-pulse ">
           <div className="bg-gray-800 p-6 rounded-lg">
             <i className="ri-smartphone-line text-yellow-500 text-2xl"></i>
